1de1a2d2fb3f59f8744a9c34a91c2014cf086781
schadinski <planlos1984@gmx.de>
2019-05-14 10:23:40 +0200

    Add ps1 script to build and execute

--- run.ps1
+++ run.ps1
@@ -1,13 +1,15 @@
-javac Client\*.java ChatApplication\*.java
-javac Server\ChatServer.java Server\ServerImpl.java ChatApplication\*.java
-javac History\ChatHistory.java History\HistoryImpl.java ChatApplication\*.java
+#Set-ExecutionPolicy RemoteSigned
+
+javac ChatClient.java ClientImpl.java ChatApplication\*.java
+javac ChatServer.java ServerImpl.java ChatApplication\*.java
+javac ChatHistory.java HistoryImpl.java ChatApplication\*.java
 
 Write-Host "Compile end"
 
 start orbd "-ORBInitialPort 1050 -ORBInitialHost localhost"
 Write-Host "orb started"
-#start java .\Server\ChatServer "-ORBInitialPort 1050 -ORBInitialHost localhost"
-#Write-Host "Server started"
-#start java History\ChatHistory "-ORBInitialPort 1050 -ORBInitialHost localhost"
-#Write-Host "HistoryServer started"
-#java  Client\ChatClient "-ORBInitialPort 1050 -ORBInitialHost localhost"
+start-process java -ArgumentList 'ChatServer', ' -ORBInitialPort 1050', ' -ORBInitialHost localhost'
+Write-Host "Server started"
+start-process java -ArgumentList 'ChatHistory', '-ORBInitialPort 1050', '-ORBInitialHost localhost'
+Write-Host "HistoryServer started"
+start-process java -ArgumentList 'ChatClient', '-ORBInitialPort 1050', '-ORBInitialHost localhost'
